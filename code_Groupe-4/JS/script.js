0eaf2edb1a0bdab8d60ca19dfcb8c3487afb62ed
GROSSMANN--LE MAUGUEN Arthur <77300083+arthur5775@users.noreply.github.com>
2025-03-09 10:22:34 +0000

    Delete code_Groupe-4 directory

--- code_Groupe-4/JS/script.js
+++
@@ -1,7 +0,0 @@
-// script.js
-document.addEventListener('mousemove', function(e) {
-    var cursor = document.querySelector('.custom-cursor');
-    cursor.style.top = e.clientY + 'px';
-    cursor.style.left = e.clientX + 'px';
-  });
-  
--- code_Groupe-4/PHP/constant.php
+++
@@ -1,7 +0,0 @@
-<?php
-    define("DB_USER", "postgres");
-    define("DB_PASSWORD", "isen44");
-    define("DB_NAME", "arbre");
-    define("DB_SERVER" , "127.0.0.1");
-    define("DB_PORT" , "5432");
-?>
